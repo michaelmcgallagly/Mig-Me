e4f98373ff1518a2efbaa5c08cdbda3a4edf9945
Michael McGallagly <mcgallagly@gmail.com>
2024-09-11 15:54:26 +0100

    Logo and Chat.jsx
    Added in the logo, as well as adding in the header for the Chat.jsx component

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -1,5 +1,15 @@
 export default function Chat() {
   return (
-    <div className="chat"></div>
+    <div className="flex-grow flex flex-col h-full border-l border-r border-[#D5A9A9]">
+        <div className="p-5 flex items-center justify-between border-b border-[#D5A9A9]" >
+            <div className="flex items-center gap-5">
+                <img src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
+                <div className="flex flex-col gap-2">
+                    <span>Jane Doe</span>                    
+                </div>
+            </div>
+        </div>
+    </div>
+    
   )
 }
--- src/Components/list/userInfo/UserInfo.jsx
+++ src/Components/list/userInfo/UserInfo.jsx
@@ -1,10 +1,13 @@
 export default function UserInfo() {
   return (
-    <div className="p-5 flex items-center justify-between">
+    <div className="p-5 flex items-center justify-between border-b border-[#D5A9A9]">
         <div className="flex items-center gap-5">
             <img src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
             <h2>John Doe</h2>
+
         </div>
+        <button className="px-5 py-2.5 bg-[#C62828] bg-opacity-55 text-white border-none rounded-md cursor-pointer transition-colors duration-300 hover:bg-red-700">LogOut</button>
+
     </div>
   )
 }
