a43c5faf19f962fb84341c42f7325ff2eefc9785
Michael McGallagly <mcgallagly@gmail.com>
2024-09-11 18:54:55 +0100

    completed firebase file
    completed firebase file to fetch all necessary data

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,11 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBic8vZs20ksS63GScB0wXqQSDqnYoCmcI",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "migme-be891.firebaseapp.com",
   projectId: "migme-be891",
   storageBucket: "migme-be891.appspot.com",
@@ -15,3 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
