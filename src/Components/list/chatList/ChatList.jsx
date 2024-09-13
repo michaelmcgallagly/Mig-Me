16446e1ab00d0a2198dd61344c5e4b80bdc7e571
Michael McGallagly <mcgallagly@gmail.com>
2024-09-13 13:49:53 +0100

    Fetching chats
    Fetching and displaying chats for the user

--- src/Components/list/chatList/ChatList.jsx
+++ src/Components/list/chatList/ChatList.jsx
@@ -1,6 +1,24 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import { useUserStore } from "../../../lib/userStore"
 import "./chatList.css"
+import { db } from "../../../lib/firebase";
 
 export default function ChatList() {
+
+  const [chats,setChats] = useState([]);
+
+  const currentUser = useUserStore();
+
+  useEffect(() =>{
+    const unSub = onSnapshot(doc(db,"userchats", currentUser.id), (doc) =>{
+      setChats(doc.data());
+    });
+
+    return () =>{
+      unSub();
+    }
+  },[currentUser.id])
+
   return (
     <div className="chatList">
         <div className="flex items-center gap-5 p-5">
@@ -12,54 +30,16 @@ export default function ChatList() {
 
         </div>
 
-
-        <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-[#D5A9A9]">
-        <img src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
-        <div className="flex flex-col gap-2">
-          <span>Jane Doe</span>
-          <p className="font-light text-sm">Hello</p>
+        {chats.map((chat)=>(
+          <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-[#D5A9A9]" key={chat.chatId}>
+          <img src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
+          <div className="flex flex-col gap-2">
+            <span>Jane Doe</span>
+            <p className="font-light text-sm">{chat.lastMessage}</p>
+          </div>
         </div>
-      </div>
-
-      <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-[#D5A9A9]">
-        <img src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
-        <div className="flex flex-col gap-2">
-          <span>Jane Doe</span>
-          <p className="font-light text-sm">Hello</p>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-[#D5A9A9]">
-        <img src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
-        <div className="flex flex-col gap-2">
-          <span>Jane Doe</span>
-          <p className="font-light text-sm">Hello</p>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-[#D5A9A9]">
-        <img src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
-        <div className="flex flex-col gap-2">
-          <span>Jane Doe</span>
-          <p className="font-light text-sm">Hello</p>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-[#D5A9A9]">
-        <img src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
-        <div className="flex flex-col gap-2">
-          <span>Jane Doe</span>
-          <p className="font-light text-sm">Hello</p>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-[#D5A9A9]">
-        <img src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
-        <div className="flex flex-col gap-2">
-          <span>Jane Doe</span>
-          <p className="font-light text-sm">Hello</p>
-        </div>
-      </div>
+        ))}
+        
 
       
 
