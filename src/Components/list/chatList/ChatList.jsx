6cdba3cf53f0b40506bc79a146baf692f1b363d1
Michael McGallagly <mcgallagly@gmail.com>
2024-09-15 17:01:05 +0100

    Search Method
    Added in a way for users to search their chats to find a specific username

--- src/Components/list/chatList/ChatList.jsx
+++ src/Components/list/chatList/ChatList.jsx
@@ -11,6 +11,7 @@ export default function ChatList() {
 
   const [chats,setChats] = useState([]);
   const [addMode, setAddMode] = useState(false);
+  const [input, setInput] = useState("");
 
   const {currentUser} = useUserStore();
   const {chatId, changeChat} = doFetchChats();
@@ -65,18 +66,20 @@ export default function ChatList() {
 
   }
 
+  const filteredChats = chats.filter(ch => ch.user.username.toLowerCase().includes(input.toLowerCase()))
+
   return (
     <div className="chatList">
         <div className="flex items-center gap-5 p-5">
             <div className="flex-1 bg-[#8c52ff] bg-opacity-50 backdrop-blur-lg backdrop-saturate-150 rounded-lg border border-white border-opacity-10 flex items-center gap-5 rounded-lg p-2">
                 <i className="fa-solid fa-magnifying-glass"></i>
-                 <input type="text" placeholder="Search" className="bg-transparent border-none outline-none text-white placeholder-white flex-1"/>
+                 <input type="text" placeholder="Search" className="bg-transparent border-none outline-none text-white placeholder-white flex-1" onChange={(e)=>setInput(e.target.value)}/>
             </div>
          <i className={addMode ? "fa-solid fa-minus cursor-pointer" : "fa-solid fa-plus cursor-pointer"} onClick={()=> setAddMode((prev)=> !prev)}></i>
 
         </div>
 
-        {chats.map((chat)=>(
+        {filteredChats.map((chat)=>(
           <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-[#D5A9A9]" key={chat.chatId} onClick={()=> handleSelect(chat)} style={{backgroundColor: chat?.isSeen ? "transparent" : "#ff5757"}}>
           <img src={chat.user.avatar ||"https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg"} alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
           <div className="flex flex-col gap-2">
