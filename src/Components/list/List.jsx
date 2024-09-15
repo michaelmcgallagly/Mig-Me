524ec8d73f0919133e2058ef1184aa7172c3bf1b
Michael McGallagly <mcgallagly@gmail.com>
2024-09-15 19:18:06 +0100

    Updated responsiveness
    Made sure the website looked good on different screen sizes

--- src/Components/list/List.jsx
+++ src/Components/list/List.jsx
@@ -4,7 +4,7 @@ import ChatList from "./chatList/ChatList.jsx"
 
 export default function List() {
   return (
-    <div className="flex-1 flex flex-col">
+    <div className="flex-1 flex flex-col border-b border-white">
         <UserInfo/>
         <ChatList/>
     </div>
--- src/Components/list/userInfo/UserInfo.jsx
+++ src/Components/list/userInfo/UserInfo.jsx
@@ -5,7 +5,7 @@ export default function UserInfo() {
 
   const {currentUser} = useUserStore();
   return (
-    <div className="p-5 flex items-center justify-between border-b border-[#D5A9A9]">
+    <div className="p-3 md:p-5 flex items-center justify-between border-b border-[#D5A9A9]">
         <div className="flex items-center gap-5">
             <img src={currentUser.avatar || "https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg"} alt="user profile picture" className="w-12 h-12 rounded-full object-cover"/>
             <h2>{currentUser.username}</h2>
