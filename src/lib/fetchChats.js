a5ab51d824fe2bb73bc9c5cbaaaeca38c5a744c9
Michael McGallagly <mcgallagly@gmail.com>
2024-09-20 15:22:14 +0100

    fetchChats.js comments

--- src/lib/fetchChats.js
+++ src/lib/fetchChats.js
@@ -1,17 +1,27 @@
 import {create} from 'zustand'
 import { useUserStore } from './userStore';
 
+//use Zustand for managing chat state
 export const doFetchChats = create((set) => ({
+
+    // chatId and user are null to indicate no active chat
     chatId:null,
     user:null,
-    changeChat:(chatId,user)=>{
-        const currentUser = useUserStore.getState().currentUser;
 
+    //this function changes the users chat
+    changeChat:(chatId,user)=>{
+
+        //fetches the currentUser
+        const currentUser = useUserStore.getState().currentUser;
+        
+        //if no chatId is provided set chatId and user to null
         if (!chatId) {
             set({
               chatId: null,
               user: null,
             });
+
+            //if a valid chatId is provided, set the chatId and user to their new values
           } else {
             set({
               chatId,
